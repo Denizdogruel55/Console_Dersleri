e262140f7867a355b6054371519947f8a9144a06
denizdogruel00 <denizyazilm@gmail.com>
2025-09-30 08:41:47 +0300

    Add project files.

---
+++ Console_Dersleri/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Dersleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Merhaba Dunya");
+            //Console.Write("Merhaba benim adim Deniz Doğruel");
+            //Console.WriteLine("mesleğim bilgisayar programcısı");
+            //Console.WriteLine("Kişi Tanıtım Karti");
+            //Console.WriteLine();
+            //Console.WriteLine("Adiniz: Deniz");
+            //Console.WriteLine("Soyadiniz:Doğruel");
+            //Console.WriteLine("Mesleğim:Yazılım muhendisi");
+            //string city;
+            //city = "Dublin";
+            //Console.WriteLine(city);
+            //string capital, country;
+            //capital = "Ankara";
+            //country = "Türkiye";
+            //Console.WriteLine(capital + " " + country);
+            //string nameSurname = "Deniz Doğruel";
+            //Console.WriteLine(nameSurname);
+            //string job;
+            //job = "Saftware Engineer";
+            //Console.WriteLine(job);
+            //int number;
+            //number = 20;
+            //Console.WriteLine(number);
+            //int number1, number2, total;
+            //number1 = 20;
+            //number2 = 20;
+            //total = number1 + number2;
+            //Console.WriteLine(total);
+            //int number1, number2,addition,extraction,multiply,division;
+            //number1 = 40;
+            //number2 = 20;
+            //addition =number1 + number2;
+            //extraction =number1 - number2;
+            //multiply =number2 * number1;
+            //division =number1 / number2;
+            //Console.WriteLine("Toplam: " + addition);
+            //Console.WriteLine("Fark: " + extraction);
+            //Console.WriteLine("Carpim: " + multiply);
+            //Console.WriteLine("Bölüm: " + division);
+            //Console.Write("Lütfen Adinizi Giriniz: ");
+            //string ad;
+            //ad = Console.ReadLine();
+            //Console.Write("HoşGeldiniz,"+ad);
+            //string ulke, sehir;
+            //Console.Write("Ulke:");
+            //ulke = Console.ReadLine();
+            //Console.Write("Sehir: ");
+            //sehir = Console.ReadLine();
+            //Console.Write(ulke + "-" + sehir);
+            //int sayi;
+            //Console.Write("Lütfen sayi Giriniz: ");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(sayi);
+            //int s1, s2, sonuc;
+            //Console.Write("Lütfen 1.sayiyi Giriniz: ");
+            //s1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Lütfen 2.sayiyi Giriniz: ");
+            //s2 = Convert.ToInt32(Console.ReadLine());
+            //sonuc = s1 + s2;
+            //Console.WriteLine("Toplam: " + sonuc);
+            //int kisa, uzun, alan, cevre;
+            //Console.Write("Kisa kenari Giriniz: ");
+            //kisa=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Uzun kenari Giriniz: ");
+            //uzun=Convert.ToInt32(Console.ReadLine());
+            //cevre=(kisa+uzun)*2;
+            //alan = kisa * uzun;
+            //Console.WriteLine("Alan: " + alan);
+            //Console.WriteLine("Cevre: "+cevre);
+            //double sayi1;
+            //sayi1 = 4.085;
+            //Console.WriteLine(sayi1);
+            //double sinav1, sinav2, sinav3, ortalama;
+            //Console.Write("Sinav1: ");
+            //sinav1=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Sinav2: ");
+            //sinav2=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Sinav3: ");
+            //sinav3=Convert.ToDouble(Console.ReadLine());
+            //ortalama=(sinav1+sinav2+sinav3)/3;
+            //Console.WriteLine("Ortalama: "+ortalama);
+            //char sehir;
+            //sehir = 'a';
+            //Console.WriteLine(sehir);
+            //char karakter;
+            //Console.Write("Karakter Giriniz: ");
+            //karakter = Convert.ToChar(Console.ReadLine());
+            //Console.Write(karakter);
+            Console.ReadLine();
+        }
+    }
+}
